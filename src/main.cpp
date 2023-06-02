d6f260d28d62f2b848ccef20b852af5304fa72bb
Thibdumont <thibdumont@gmail.com>
2023-06-02 21:17:45 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Hello");
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+  return x + y;
+}
